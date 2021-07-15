83cca96fc14e0457b912b541a2aee78d0002d43f
lonnalu <lonnalu@gmail.com>
2021-07-15 14:07:48 -0700

    updated animation so it will not be used when user reaches rip or final screen

--- scripts/app.js
+++ scripts/app.js
@@ -100,12 +100,10 @@ const game = {
         $('#food-score').text(`0`);
         $('#main-image img').attr('src', './images/pig-rip.png');
         $('#main-image').after('<button>RESTART GAME</button>');
+        $('#main-image img').removeAttr('class','animation');
         $('button').on('click', function(){
             location.reload();
         });
-            //     $('#restart').on('click', function(){ NEED TO TEST!!!
-            // location.reload();
-        
     },
     
     transferStage() {
@@ -123,6 +121,7 @@ const game = {
     breakfast(){
         $('#main-image img').attr('src', './images/pig-breakfast.jpg');
         $('#transfer').remove();
+        $('#main-image img').removeAttr('class','animation');
         $('#main-image').append(`<div><p>Yum! Breakfast was delicious. Thank you ${game.username} for supplying an unforgettable meal!</p><br><button id="restart">RESTART GAME</button></div>`);
         $('#restart').on('click', function(){
             location.reload();
